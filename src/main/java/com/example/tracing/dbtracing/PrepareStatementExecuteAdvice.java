6e43b9a8287bda4e858b60bae079655a26ba0dee
김유성 <helikim1127@lguplus.co.kr>
2025-04-01 15:05:15 +0900

    [feat]  db tracing - generate select query

--- src/main/java/com/example/tracing/dbtracing/PrepareStatementExecuteAdvice.java
+++ src/main/java/com/example/tracing/dbtracing/PrepareStatementExecuteAdvice.java
@@ -3,45 +3,131 @@ package com.example.tracing.dbtracing;
 import com.example.tracing.logging.DynamicLogFileGenerator;
 import net.bytebuddy.asm.Advice;
 
+import java.io.File;
+import java.lang.reflect.Method;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import static com.example.tracing.dbtracing.PrepareStatementExecuteHandler.exportToFile;
+import static com.example.tracing.dbtracing.QueryGenerator.generateSelectQuery;
 
 public class PrepareStatementExecuteAdvice {
+    public static String previousQuery = "";
+    public static final ThreadLocal<Boolean> isInternal = ThreadLocal.withInitial(() -> false);
 
     @Advice.OnMethodEnter
-    public static void onEnter(@Advice.This PreparedStatement stmt) {
+    public static void onEnter(@Advice.This PreparedStatement stmt,
+                               @Advice.Origin Method method,
+                               @Advice.AllArguments Object[] args) {
         try {
-            String rawSql = stmt.toString();
-            String sql = SqlUtils.extractSqlFromPreparedStatement(rawSql);
+            boolean isTarget = isTargetQuery(stmt);
 
-            System.out.println("[Agent - DB] Executing SQL: " + sql);
-            DynamicLogFileGenerator.log("[Agent - DB] Executing SQL: " + sql);
-//
-//            String selectQuery = QueryGenerator.generateSelectQuery(sql);
-//            if (selectQuery == null) {
-//                System.out.println("[Agent] 전 쿼리 생성 불가");
-//                return;
-//            }
-//
-//            System.out.println("[Agent - DB] 전 쿼리용 SQL: " + selectQuery);
-//
-//
-//            try (PreparedStatement preStmt = stmt.getConnection().prepareStatement(selectQuery)) {
-//                for (int i = 0; i < args.length; i++) {
-//                    preStmt.setObject(i + 1, args[i]);
-//                }
-//
-//                try (ResultSet rs = preStmt.executeQuery()) {
-//                    int count = 0;
-//                    while (rs.next()) {
-//                        count++;
-//                    }
-//                    System.out.println("[Agent] 전 쿼리 결과 row 수: " + count);
-//                }
-//            }
+            if (isTarget) {
+                boolean needRollback = needRollback(method);
+                logQueryDetails(stmt, args); // 전 쿼리용 SELECT 실행 및 row 수 로깅
 
+                if (needRollback) {
+                    stmt.getConnection().rollback();
+                } else {
+                    stmt.getConnection().commit();
+                }
+            }
         } catch (Exception e) {
-            System.err.println("[Agent - DB] SQL 로깅 실패: " + e.getMessage());
+            System.out.println("[Agent] Advice 실행 중 예외 발생");
         }
     }
+
+    public static boolean needRollback(Method method) {
+        return !method.getName().equals("executeQuery");
+    }
+
+//    public static boolean isInRequestScope() {
+//        return RequestContextHolder.getRequestAttributes() != null;
+//    }
+
+    public static boolean isTargetQuery(PreparedStatement stmt) {
+        String sql = SqlUtils.extractSqlFromPreparedStatement(stmt.toString()).toLowerCase();
+        return !sql.contains("hibernate_sequence") &&
+                !sql.contains(".message") &&
+                !sql.contains("_seq") &&
+                !sql.contains("count(*)") &&
+                !sql.equals(previousQuery);
+    }
+
+    public static void logQueryDetails(PreparedStatement stmt, Object[] args) {
+        if (isInternal.get()) return;
+        try {
+            isInternal.set(true);
+            String sql = SqlUtils.extractSqlFromPreparedStatement(stmt.toString());
+            previousQuery = sql.replaceAll("\\r?\\n", "");
+            DynamicLogFileGenerator.log("[Agent] Executing SQL: " + previousQuery);
+
+            // 전 쿼리 SELECT 생성 및 실행, 파일 경로 반환
+            String writtenBlobFilePath = generatePreProcessingQuery(stmt, args, sql);
+
+            // 마지막에 로그 출력은 여기서
+            DynamicLogFileGenerator.log("Prep query: " + writtenBlobFilePath);
+
+        } catch (Exception e) {
+            System.out.println("[Agent] 전 쿼리 로깅 실패");
+        } finally {
+            isInternal.set(false);  // ✅ 플래그 해제
+        }
+    }
+
+    public static String generatePreProcessingQuery(PreparedStatement stmt, Object[] args, String sql) {
+        String writtenBlobFilePath = "";
+
+        try {
+            if (sql.toLowerCase().contains("insert")) {
+                return writtenBlobFilePath;
+            }
+
+            // 전 쿼리용 SELECT 생성
+            String selectQuery = generateSelectQuery(sql);
+            if (selectQuery == null) {
+                System.out.println("[Agent] 전 쿼리 SELECT 생성 실패");
+                return writtenBlobFilePath;
+            }
+
+            DynamicLogFileGenerator.log("[Agent] Generated Select Query: " + selectQuery);
+
+            try (PreparedStatement selectStmt = stmt.getConnection().prepareStatement(
+                    selectQuery,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY)) {
+
+                if (args != null) {
+                    for (int i = 0; i < args.length; i++) {
+                        selectStmt.setObject(i + 1, args[i]);
+                    }
+                }
+
+                try (ResultSet rs = selectStmt.executeQuery()) {
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int rsLength = 0;
+                    while (rs.next()) rsLength++;
+
+                    rs.beforeFirst();
+                    System.out.println("ResultSet has " + rsLength + " rows before populate()");
+                    System.out.println("ResultSet is " + rs);
+
+                    if (rsLength > 0) {
+                        String fileName = "blob-" + System.currentTimeMillis() + ".dat";
+                        File blobFile = new File("logs/", fileName);
+                        writtenBlobFilePath = exportToFile(blobFile, rs);
+                    }
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("[Agent] 전 쿼리 처리 실패: " + e.getMessage());
+        }
+
+        return writtenBlobFilePath;
+    }
+
 }
+
--- src/main/java/com/example/tracing/dbtracing/QueryGenerator.java
+++ src/main/java/com/example/tracing/dbtracing/QueryGenerator.java
@@ -1,35 +1,61 @@
 package com.example.tracing.dbtracing;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class QueryGenerator {
     public static String generateSelectQuery(String sql) {
+        // update/delete query 에서 select 쿼리로 변환하여 현재값 알기
         sql = sql.toLowerCase();
         if (sql.startsWith("update")) {
-            String table = getTableNameFromUpdate(sql);
+            System.out.println("update query!!!!!");
+            String table = getTableNameFromUpdateQuery(sql);
             String where = getWhereClause(sql);
             return (table != null && where != null) ? "SELECT * FROM " + table + " " + where : null;
         } else if (sql.startsWith("delete")) {
-            String table = getTableNameFromDelete(sql);
+            System.out.println("delete query!!!!!");
+            String table = getTableNameFromDeleteQuery(sql);
             String where = getWhereClause(sql);
             return (table != null && where != null) ? "SELECT * FROM " + table + " " + where : null;
         } else if (sql.startsWith("select")) {
-            return sql;
+            System.out.println("select query!!!!!");
+
+            return getAllColumnFromSelectQuery(sql);
         }
         return null;
     }
 
-    private static String getTableNameFromUpdate(String sql) {
+    public static String getAllColumnFromSelectQuery(String sql) {
+        if (sql == null) return null;
+
+        try {
+            String regex = "(?i)select (.*?) from";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(sql);
+
+            if (matcher.find()) {
+                return matcher.replaceAll("select * from");
+            }
+        } catch (Exception e) {
+            System.err.println("[SqlUtils] SELECT * 변환 실패: " + e.getMessage());
+        }
+        return null;
+    }
+
+
+    public static String getTableNameFromUpdateQuery(String sql) {
         int start = sql.indexOf("update") + 6;
         int end = sql.indexOf("set");
         return (start < end && start > 0 && end > 0) ? sql.substring(start, end).trim() : null;
     }
 
-    private static String getTableNameFromDelete(String sql) {
+    public static String getTableNameFromDeleteQuery(String sql) {
         int start = sql.indexOf("delete from") + 11;
         int end = sql.indexOf("where");
         return (start < end && start > 0 && end > 0) ? sql.substring(start, end).trim() : null;
     }
 
-    private static String getWhereClause(String sql) {
+    public static String getWhereClause(String sql) {
         int index = sql.indexOf("where");
         return index > 0 ? sql.substring(index).trim() : null;
     }
