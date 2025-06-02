a3bf157fc5e4ca025ceb25f3b23a24a1b483bdf4
김유성 <helikim1127@lguplus.co.kr>
2025-06-02 10:37:38 +0900

    [feat] socket port 추가

--- agent-main/src/main/java/com/example/agentMain/tracing/sockettracing/SocketInterceptor.java
+++ agent-main/src/main/java/com/example/agentMain/tracing/sockettracing/SocketInterceptor.java
@@ -33,7 +33,7 @@ public class SocketInterceptor {
             SocketLogContext.setFileName(fileName); // 스레드별 고유 파일명 등록
 
             System.out.println("[Agent Socket] " + ip + " 원하는 소켓 ip 호출됨!!!");
-            DynamicLogFileGenerator.log("Socket: " + fileName);
+            DynamicLogFileGenerator.log("SocketInterceptor: " + fileName);
         }
     }
 }
